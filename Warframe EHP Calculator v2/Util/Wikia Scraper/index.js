b253f606b53d335bc6ec30d2a24f7a8882b9b3b5
Bradley Treweek <cmd430@gmail.com>
2019-06-21 16:15:34 +1200

    updated wiki scraper to log in output window

--- Warframe EHP Calculator v2/Util/Wikia Scraper/index.js
+++ Warframe EHP Calculator v2/Util/Wikia Scraper/index.js
@@ -1,6 +1,7 @@
 const tabletojson = require('tabletojson')
 const fs = require('fs')
 
+console.log('Updating Warframe Data from \'https://warframe.fandom.com/wiki/Warframes_Comparison/Stats\'')
 tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/Stats', {
     useFirstRowForHeadings: true
   },
@@ -16,7 +17,8 @@ tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/St
     let rank30 = tablesAsJson[1]
     let warframes = {}
     unranked.forEach((warframe, index) => {
-      if(!warframe.Name.includes(' Umbra Prime')) {
+      if (!warframe.Name.includes(' Umbra Prime') && warframe.Name !== 'Name') {
+        console.log('Processing Warframe Data for ' + warframe.Name)
         warframe.Armor = (warframe.Armor === "-" ? 0 : warframe.Armor)
         warframe.Health = (warframe.Health === "-" ? 0 : warframe.Health)
         warframe.Shields = (warframe.Shields === "-" ? 0 : warframe.Shields)
@@ -85,42 +87,51 @@ tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/St
     // Output XML
     let str = `<?xml version="1.0" encoding="utf-8" ?>\r\n<warframes>\r\n`
     Object.keys(warframes).forEach(warframe => {
-      if (warframe !== "Name") {
-        str = str + `  <warframe name="${warframe}">\r\n`
-        str = str + `    <variants>\r\n`
-        str = str + `      <variant name="base" armor="${warframes[warframe].variants.base.armor}" health="${warframes[warframe].variants.base.health}" shield="${warframes[warframe].variants.base.shield}" energy="${warframes[warframe].variants.base.energy}" strength="${warframes[warframe].variants.base.strength}" />\r\n`
-        if (warframes[warframe].variants.prime) {
-          str = str + `      <variant name="prime" armor="${warframes[warframe].variants.prime.armor}" health="${warframes[warframe].variants.prime.health}" shield="${warframes[warframe].variants.prime.shield}" energy="${warframes[warframe].variants.prime.energy}" strength="${warframes[warframe].variants.prime.strength}" />\r\n`
-        }
-        if (warframes[warframe].variants.umbra) {
-          str = str + `      <variant name="umbra" armor="${warframes[warframe].variants.umbra.armor}" health="${warframes[warframe].variants.umbra.health}" shield="${warframes[warframe].variants.umbra.shield}" energy="${warframes[warframe].variants.umbra.energy}" strength="${warframes[warframe].variants.umbra.strength}" />\r\n`
-        }
-        str = str + `    </variants>\r\n`
-        if (warframes[warframe].rank_multipliers) {
-          str = str + `    <rank_multipliers>\r\n`
-          if (warframes[warframe].rank_multipliers.armor) {
-            str = str + `      <rank_multiplier name="armor" multiplier="${warframes[warframe].rank_multipliers.armor}" />\r\n`
-          }
-          if (warframes[warframe].rank_multipliers.health) {
-            str = str + `      <rank_multiplier name="health" multiplier="${warframes[warframe].rank_multipliers.health}" />\r\n`
-          }
-          if (warframes[warframe].rank_multipliers.shield) {
-            str = str + `      <rank_multiplier name="shield" multiplier="${warframes[warframe].rank_multipliers.shield}" />\r\n`
-          }
-          if (warframes[warframe].rank_multipliers.energy) {
-            str = str + `      <rank_multiplier name="energy" multiplier="${warframes[warframe].rank_multipliers.energy}" />\r\n`
-          }
-          if (warframes[warframe].rank_multipliers.strength) {
-            str = str + `      <rank_multiplier name="strength" multiplier="${warframes[warframe].rank_multipliers.strength}" />\r\n`
-          }
-          str = str + `    </rank_multipliers>\r\n`
-        }
-        str = str + `  </warframe>\r\n`
+      str = str + `  <warframe name="${warframe}">\r\n`
+      str = str + `    <variants>\r\n`
+      str = str + `      <variant name="base" armor="${warframes[warframe].variants.base.armor}" health="${warframes[warframe].variants.base.health}" shield="${warframes[warframe].variants.base.shield}" energy="${warframes[warframe].variants.base.energy}" strength="${warframes[warframe].variants.base.strength}" />\r\n`
+      if (warframes[warframe].variants.prime) {
+        str = str + `      <variant name="prime" armor="${warframes[warframe].variants.prime.armor}" health="${warframes[warframe].variants.prime.health}" shield="${warframes[warframe].variants.prime.shield}" energy="${warframes[warframe].variants.prime.energy}" strength="${warframes[warframe].variants.prime.strength}" />\r\n`
       }
+      if (warframes[warframe].variants.umbra) {
+        str = str + `      <variant name="umbra" armor="${warframes[warframe].variants.umbra.armor}" health="${warframes[warframe].variants.umbra.health}" shield="${warframes[warframe].variants.umbra.shield}" energy="${warframes[warframe].variants.umbra.energy}" strength="${warframes[warframe].variants.umbra.strength}" />\r\n`
+      }
+      str = str + `    </variants>\r\n`
+      if (warframes[warframe].rank_multipliers) {
+        str = str + `    <rank_multipliers>\r\n`
+        if (warframes[warframe].rank_multipliers.armor) {
+          str = str + `      <rank_multiplier name="armor" multiplier="${warframes[warframe].rank_multipliers.armor}" />\r\n`
+        }
+        if (warframes[warframe].rank_multipliers.health) {
+          str = str + `      <rank_multiplier name="health" multiplier="${warframes[warframe].rank_multipliers.health}" />\r\n`
+        }
+        if (warframes[warframe].rank_multipliers.shield) {
+          str = str + `      <rank_multiplier name="shield" multiplier="${warframes[warframe].rank_multipliers.shield}" />\r\n`
+        }
+        if (warframes[warframe].rank_multipliers.energy) {
+          str = str + `      <rank_multiplier name="energy" multiplier="${warframes[warframe].rank_multipliers.energy}" />\r\n`
+        }
+        if (warframes[warframe].rank_multipliers.strength) {
+          str = str + `      <rank_multiplier name="strength" multiplier="${warframes[warframe].rank_multipliers.strength}" />\r\n`
+        }
+        str = str + `    </rank_multipliers>\r\n`
+      }
+      str = str + `  </warframe>\r\n`
     })
     str = str + `</warframes>`
-    fs.writeFile('../../Data/Warframes.xml', str, err => {
-      if (err) {}
+    fs.readFile('../../Data/Warframes.xml', 'utf8', (err, data) => {
+      if (!err && data === str) {
+          console.log('Warframes.xml already up to date')
+      } else {
+        fs.writeFile('../../Data/Warframes.xml', str, err => {
+          if (err) {
+            console.log('Warframes.xml could not be Updated')
+            console.error(err.message)
+          } else {
+            console.log('Warframes.xml has been Updated')
+          }
+        })
+      }
     })
   }
 )
